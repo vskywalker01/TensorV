0ab28fc1b781d30f28f95d3a713f61aa99d4976a
vskywalker01 <vittoriopolci@live.com>
2026-06-05 17:56:24 +0200

    bugfix + testbench for MAC

--- ProcessingElement/MAC/src/MAC_stage3.vhd
+++ ProcessingElement/MAC/src/MAC_stage3.vhd
@@ -18,13 +18,14 @@ entity MAC_stage3 is
     );
 end MAC_stage3;
 
-architecture Behavioral of MAC_stage3 is 
-    type ACCUMULATION_PARTIALS is ARRAY (natural range <>) of STD_LOGIC_VECTOR(ACC_SIZE-1 downto 0);
+architecture Behavioral of MAC_stage3 is     
     
-    signal partial1: SIGNED(ACC_SIZE-1 downto 0); 
-    signal partial2: SIGNED(ACC_SIZE-1 downto 0);
+    signal partial1:            SIGNED(MATRIX_OUTPUT_SIZE-1 downto 0); 
+    signal partial2:            SIGNED(MATRIX_OUTPUT_SIZE-1 downto 0);
+    signal mul_out:             SIGNED(MATRIX_OUTPUT_SIZE-1 downto 0);
+    signal mul_out_corrected:   SIGNED(ACC_SIZE-1 downto 0);
 
-    signal result: SIGNED(ACC_SIZE-1 downto 0);
+    signal result:              SIGNED(ACC_SIZE-1 downto 0);
     
 begin 
     partials_routing: for c in 0 to (MATRIX_OUTPUT_SIZE-1) generate
@@ -33,22 +34,12 @@ begin
         partial1(c) <= matrix_in(0)(c); 
         partial2(c) <= matrix_in(1)(c) when HEIGHT>1 else '0';
     end generate; 
-    matrix_result(MATRIX_OUTPUT_SIZE-1 downto 0) <= partial1+partial2;
-    
-    mul_extension: for c in 0 to (MATRIX_OUTPUT_SIZE-1) generate
-        constant HEIGHT: INTEGER := get_matrix_column_height(c,2);
-    begin
-        lower_bits: if (c<MATRIX_OUTPUT_SIZE-1) generate
-        partial1(c) <= matrix_in(0)(c); 
-        partial2(c) <= matrix_in(1)(c) when HEIGHT>1 else '0';
-    end generate; 
-    
-    
-    sign_extension: for c in MATRIX_OUTPUT_SIZE to (ACC_SIZE-1) generate
-        mul_result(c) <= mul_result(MATRIX_OUTPUT_SIZE-1);
-    end generate;
-    
-    result <= mul_result+SIGNED(data_acc_in);
+    mul_out <= partial1 + partial2;
+    mul_routing: for c in 0 to (ACC_SIZE-1) generate 
+        mul_out_corrected(c) <= mul_out(c) when (c<MATRIX_OUTPUT_SIZE-1) else 
+                                not(mul_out(MATRIX_OUTPUT_SIZE-1));  
+    end generate;  
+    result <= mul_out_corrected+SIGNED(data_acc_in); 
     
     pipeline_latch: process(clk) 
     begin 
--- ProcessingElement/MAC/src/tests/MAC_test.vhd
+++ ProcessingElement/MAC/src/tests/MAC_test.vhd
@@ -3,7 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -38,6 +38,9 @@ architecture Behavioral of MAC_test is
     signal data_acc_in: STD_LOGIC_VECTOR(31 downto 0); 
             
     signal r_out:STD_LOGIC_VECTOR(31 downto 0);
+    
+    shared variable acc_error_count: integer := 0;
+    shared variable mul_error_count: integer := 0;
 begin
     MAC_test: MAC 
         port map (
@@ -61,10 +64,61 @@ begin
         reset <= '1'; 
         wait for 30ns; 
         reset <= '0'; 
-        data_acc_in <= x"00000005";
-        data_a <= x"10"; -- 
-        data_b <= x"02"; -- 
-        wait for 100ns; 
+        data_acc_in <= x"00000000";
+        for i in -128 to 127 loop 
+            for j in -128 to 127 loop 
+                data_a <= STD_LOGIC_VECTOR(to_signed(i,8));
+                data_b <= STD_LOGIC_VECTOR(to_signed(j,8));
+                wait for 40ns; 
+                if not(signed(r_out) = to_signed(i,32)*to_signed(j,32)) then
+                    report "Error during estimation of 0 + (" 
+                        & integer'image(i) 
+                        & "*" 
+                        & integer'image(j) 
+                        & ") " 
+                        & "got " 
+                        & integer'image(to_integer(signed(r_out)))
+                    severity error; 
+                    mul_error_count:=mul_error_count+1;
+                end if;
+            end loop;
+        end loop;
+        
+        for j in -128 to 127 loop 
+            for i in -128 to 127 loop 
+                data_a <= STD_LOGIC_VECTOR(to_signed(i,8));
+                data_b <= STD_LOGIC_VECTOR(to_signed(i,8));
+                data_acc_in <= STD_LOGIC_VECTOR(to_signed(j,32));
+                wait for 40ns; 
+                if not(signed(r_out) = (to_signed(i,32)*to_signed(i,32))+to_signed(j,32)) then
+                    report "Error during estimation of "
+                        & integer'image(j) 
+                        & " + (" 
+                        & integer'image(i) 
+                        & "*" 
+                        & integer'image(i) 
+                        & ") got " 
+                        & integer'image(to_integer(signed(r_out)))
+                    severity error; 
+                    acc_error_count:=acc_error_count+1;
+                end if;
+            end loop;
+        end loop;
+       
+        report "=============================================";
+        report "Test terminated";
+        report "Multiplication test terminated with "  & integer'image(mul_error_count) & " errors"; 
+        report "Accumulation test terminated with "  & integer'image(acc_error_count) & " errors"; 
+        
+        if (acc_error_count+mul_error_count = 0) then
+            report "Test completed successfully! :)";
+        else 
+            report "Test failed :(";
+        end if;
+        report "=============================================";
+        
+        wait; 
+        
     end process; 
 
 end Behavioral;
